f492eee3f77d94b995f05a9621cbb94d303f7e68
Noah Leigh <noah.d.leigh@gmail.com>
2017-09-24 13:03:50 -0400

    Added link when starting server locally

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,4 +19,5 @@ app.get("/", function (request, response) {
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Visit http://localhost:' + listener.address().port + '/ to see');
 });

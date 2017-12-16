0604d864c3c333487733121e33918e2bac8dd4f8
Noah Leigh <noah.d.leigh@gmail.com>
2017-12-16 11:42:49 -0500

    eslint fixes

--- client.js
+++ client.js
@@ -6,98 +6,102 @@ const slotAlphabet = ['💖', '🐱', '✨', '🌈', '🦄', '🌼', '🐶', '�
 // Boolean for determining if spinning or not
 let spinningState = false;
 // Variable for holding the setInterval identifier
-let spinningInterval = undefined;
+let spinningInterval = 0;
 // ms between emoji change
 const spinDelay = 100;
 
 /**
- * Randomize array element order in-place.
+* Returned a shuffled array.
  * Using Durstenfeld shuffle algorithm.
  * Source: https://stackoverflow.com/a/12646864
  */
 const shuffleArray = function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
+  const shuffled = array;
+  for (let i = shuffled.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = shuffled[i];
+    shuffled[i] = shuffled[j];
+    shuffled[j] = temp;
+  }
+  return shuffled;
+};
 
 // Generator that loops through a shuffled valueArray forever
 const valueGenerator = function* valueGenerator(valueArray) {
   const shuffledValueArray = shuffleArray(valueArray.slice(0));
-  for (let index = 0; true; index++){
-    index = index % shuffledValueArray.length;
+  for (let index = 0; true; index += 1) {
+    index %= shuffledValueArray.length;
     yield shuffledValueArray[index];
   }
-}
+};
 
 // Generator that yields an array of slotValues
 const slotValuesGenerator = function* slotValuesGenerator(valueArray, length) {
-  const generators = Array(length).fill().map(() => {
-    return valueGenerator(valueArray);
-  });
-
-  while (true){
+  const generators = Array(length).fill().map(() => valueGenerator(valueArray));
+  while (true) {
     yield generators.map(gen => gen.next().value);
   }
-}
+};
 
 // Set the textContent of the elements in elemArray to the values in valArray
 const displaySlotValues = function displaySlotValues(elemArray, valArray) {
   elemArray.forEach((value, index, array) => {
-    array[index].textContent = valArray[index];
+    array[index].textContent = valArray[index]; // eslint-disable-line no-param-reassign
   });
 };
 
 // Self-calling function to display a certain number of slots a given amount of times
 // Source: https://stackoverflow.com/a/3583740
-const displaySlicedValues = function displaySlicedValues(iterations, generator, elementArray, islice, delay) {
-  displaySlotValues(elementArray.slice(islice), generator.next().value.slice(islice));
-  iterations--;
+const displaySlicedValues = function displaySlicedValues(
+  iterations,
+  generator,
+  elementArray,
+  islice,
+  delay,
+) {
   if (iterations > 0) {
+    displaySlotValues(elementArray.slice(islice), generator.next().value.slice(islice));
     setTimeout(() => {
-        displaySlicedValues(iterations, generator, elementArray, islice, delay)
+      displaySlicedValues(iterations - 1, generator, elementArray, islice, delay);
     }, delay);
   }
-}
+};
 
 // Self-calling function to stop the slots one-by-one with delay (like a slot machine!)
 // Source: https://stackoverflow.com/a/3583740
 const slowdownSpin = function slowdownSpin(slices, iterations, generator, elementArray, delay) {
-  let islice = elementArray.length - slices
-  displaySlicedValues(iterations, generator, elementArray, islice, delay)
-  slices--;
-  if (slices > 0){
+  const islice = elementArray.length - slices;
+  if (slices > 0) {
+    displaySlicedValues(iterations, generator, elementArray, islice, delay);
     setTimeout(() => {
-        slowdownSpin(slices, iterations, generator, elementArray, delay)
-    }, delay*iterations);
+      slowdownSpin(slices - 1, iterations, generator, elementArray, delay);
+    }, delay * iterations);
   }
-}
+};
 
 // Toggle the spinning of the emoji
-const toggleSpinning = function toggleSpinning(generator, elementArray){
+const toggleSpinning = function toggleSpinning(generator, elementArray) {
   if (spinningState) {
     // If spinning, stop spinning
     clearInterval(spinningInterval);
     // Wind down the slots one-by-one
-    slowdownSpin(elementArray.length - 1, 5, generator, elementArray, spinDelay)
+    slowdownSpin(elementArray.length - 1, 5, generator, elementArray, spinDelay);
     // Officially done!
     spinningState = false;
   } else {
     // If not spinning, start spinning
+    // Make a shorthand function
+    const displayValues = () => displaySlotValues(elementArray, generator.next().value);
     // Make the button more responsive by updating the emoji immediately instead of after spinDelay
-    displaySlotValues(elementArray, generator.next().value);
-    spinningInterval = setInterval(() => displaySlotValues(elementArray, generator.next().value), spinDelay);
+    displayValues();
+    spinningInterval = setInterval(displayValues, spinDelay);
     spinningState = true;
   }
 };
 
 // Stop the double-tap zoom on mobile
 // Source: https://stackoverflow.com/a/28752323
-const preventDoubleTapZoom = event => {
+const preventDoubleTapZoom = (event) => {
   event.preventDefault();
   event.target.click();
 };
@@ -111,21 +115,21 @@ const slotsGenerator = slotValuesGenerator(slotAlphabet, slotElements.length);
 // Make a button to start and stop the spinning
 const slotButton = document.getElementById('slot-button');
 slotButton.addEventListener('touchend', preventDoubleTapZoom);
-slotButton.addEventListener('click', event => {
+slotButton.addEventListener('click', (event) => {
   // Start or stop the spinning
   toggleSpinning(slotsGenerator, slotElements);
   // Change the button text
-  event.target.textContent = `${spinningState ? 'Stop' : 'Start'} Spinning!`;
+  slotButton.textContent = `${spinningState ? 'Stop' : 'Start'} Spinning!`;
   // Change the button style
   event.target.classList.toggle('italic');
 
   // Make a button to copy emoji string if it doesn't exist
-  if (!document.getElementById('copy-to-clipboard-btn')){
+  if (!document.getElementById('copy-to-clipboard-btn')) {
     const copyButton = document.createElement('button');
     copyButton.id = 'copy-to-clipboard-btn';
     copyButton.textContent = 'Copy to Clipboard 📋';
     copyButton.addEventListener('touchend', preventDoubleTapZoom);
-    copyButton.addEventListener('click', event => {
+    copyButton.addEventListener('click', () => {
       const emojiString = slotElements.map(elem => elem.textContent).join('');
       const tempInput = document.createElement('input');
       tempInput.value = emojiString;
@@ -133,10 +137,9 @@ slotButton.addEventListener('click', event => {
       tempInput.select();
       try {
         const success = document.execCommand('copy');
-        console.log(`${success ? `successful: ${emojiString}` : 'unsuccessful'}`);
-        event.target.textContent = success ? 'Copied!' : 'Couldn\'t copy 😢';
+        copyButton.textContent = success ? 'Copied!' : 'Couldn\'t copy 😢';
       } catch (err) {
-        console.log(err);
+        console.error(err); // eslint-disable-line no-console
       }
       tempInput.remove();
     });
@@ -145,7 +148,3 @@ slotButton.addEventListener('click', event => {
     document.getElementById('copy-to-clipboard-btn').textContent = 'Copy to Clipboard 📋';
   }
 });
-
-
-
-
